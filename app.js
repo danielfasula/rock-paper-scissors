a1afa1c4110bcf3f8dc1f9ee0884d185429d508e
Daniel Fasula <danielfasula@gmail.com>
2021-02-10 15:54:37 -0700

    done

--- app.js
+++ app.js
@@ -6,40 +6,27 @@ function play(playerChoice) {
 
     if (playerChoice == 'rock') {
         if (computerChoice == 'rock') {
-            // alert('You Tie')
             document.getElementById('test').textContent = 'You tie! Try Again.';
         } else if (computerChoice == 'paper') {
-            // alert('You Lose!')
             document.getElementById('test').textContent = 'You Lose! Try Again.';
         } else if (computerChoice == 'scissors') {
-            // alert('You Win!')
             document.getElementById('test').textContent = 'You Win! Your father will finally be proud.';
-
         }
     } else if (playerChoice == 'paper') {
         if (computerChoice == 'rock') {
-            // alert('You Win')
             document.getElementById('test').textContent = 'You Win! Your father will finally be proud.';
         } else if (computerChoice == 'paper') {
-            // alert('You Tie')
             document.getElementById('test').textContent = 'You Tie! Try Again.';
         } else if (computerChoice == 'scissors') {
-            // alert('You Lose!')
             document.getElementById('test').textContent = 'You Lose! Try Again.';
-
         }
     } else if (playerChoice == 'scissors') {
         if (computerChoice == 'rock') {
-            // alert('You Lose!')
             document.getElementById('test').textContent = 'You Lose! Try Again.';
         } else if (computerChoice == 'paper') {
-            // alert('You Win!')
             document.getElementById('test').textContent = 'You Win! Your father will finally be proud.';
         } else if (computerChoice == 'scissors') {
-            // alert('You Tie')
             document.getElementById('test').textContent = 'You Lose! Try Again.';
         }
     }
-
-
 }
